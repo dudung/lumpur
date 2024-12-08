12d1ba72bc5fcdb8d2b8d31ea4efd8c1031eebc0
Sparisoma Viridi <dudung@gmail.com>
2024-12-08 21:22:49 +0700

    create new files for python package

---
+++ src/lumpur/misc/info.py
@@ -0,0 +1,8 @@
+# Get abbreviation of lumpur, the package name
+# Sparisoma Viridi | https://github.com/dudung
+# 20241208v0
+def abbr():
+  return 'learn \
+  to use methods \
+  for processing \
+  unclear response'
